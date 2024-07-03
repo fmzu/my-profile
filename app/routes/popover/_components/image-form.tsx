56d469adec7bf353c244de9e71caed613a0c853f
fmzu <fjtmzk0723@gmail.com>
2024-07-03 17:59:07 +0900

    update: アップロードした画像を表示する

--- app/routes/popover/_components/image-form.tsx
+++ app/routes/popover/_components/image-form.tsx
@@ -1,10 +1,22 @@
 import { ArrowUpFromLineIcon } from "lucide-react"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 export function ImageForm() {
   const fileInputRef = useRef<HTMLInputElement>(null)
+  const [imageSrc, setImageSrc] = useState<string | null>(null)
 
   const cropperClassName = "react-easy-crop-container"
+
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files ? event.target.files[0] : null
+    if (file) {
+      const reader = new FileReader()
+      reader.onload = (e) => {
+        setImageSrc(e.target?.result as string)
+      }
+      reader.readAsDataURL(file)
+    }
+  }
 
   return (
     <div className="relative">
@@ -13,12 +25,13 @@ export function ImageForm() {
         <div className="text-center text-sm opacity-80">
           {"画像アップロード"}
         </div>
+        {imageSrc && <img src={imageSrc} alt="Uploaded" className="mt-4" />}
       </div>
       <input
         ref={fileInputRef}
         type="file"
         accept=".webp,.png,.jpeg,.jpg,.gif,.svg,.bmp,.ico,.tiff,.tif,.svgz,.apng,.avif,.jfif,.pjpeg,.pjp,.jpgv,.hdp,.jpe,.jpeg2000,.jxr,.wdp,.jng,.jif,.jfi"
-        // onChange={onFileChange}
+        onChange={onFileChange}
         className="absolute top-0 left-0 h-full w-full cursor-pointer opacity-0"
       />
     </div>
