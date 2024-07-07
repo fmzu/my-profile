50a4766a48d9ffbee6e68a39647f69211f9e7079
fmzu <149039422+fmzu@users.noreply.github.com>
2024-07-07 16:55:01 +0900

    update: フォントを変更する

--- app/root.tsx
+++ app/root.tsx
@@ -4,27 +4,37 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+} from "@remix-run/react"
+import "./tailwind.css"
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Yomogi&display=swap"
+          rel="stylesheet"
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="font-Raleway">
         {children}
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
+import { fontFamily } from "tailwindcss/defaultTheme"
 
 export default {
-	content: ["./app/**/*.{js,jsx,ts,tsx}"],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-} satisfies Config;
+  content: ["./app/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Yomogi"', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
