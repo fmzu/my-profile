77f96a364c3ae028efe76972fd5f8a608b9a8d45
fmzu <fjtmzk0723@gmail.com>
2024-07-17 16:50:57 +0900

    update: コンポーネントを追加する

--- app/routes/popover/_components/color-selects.tsx
+++ app/routes/popover/_components/color-selects.tsx
@@ -8,50 +8,27 @@ type Props = {
 }
 
 export function ColorSelects(props: Props) {
+  const colors = [
+    { bgColor: "bg-yellow-300", inputColor: "bg-yellow-200" },
+    { bgColor: "bg-lime-300", inputColor: "bg-lime-200" },
+    { bgColor: "bg-teal-300", inputColor: "bg-teal-200" },
+    { bgColor: "bg-sky-300", inputColor: "bg-sky-200" },
+    { bgColor: "bg-violet-300", inputColor: "bg-violet-200" },
+    { bgColor: "bg-pink-300", inputColor: "bg-pink-200" },
+    { bgColor: "bg-red-300", inputColor: "bg-red-200" },
+  ]
+
   return (
     <div className="grid grid-cols-7 gap-2">
-      <ColorSelectButton
-        bgColor={"bg-yellow-300"}
-        inputColor={"bg-yellow-200"}
-        setBgColor={props.setBgColor}
-        setInputColor={props.setInputColor}
-      />
-      <ColorSelectButton
-        bgColor={"bg-lime-300"}
-        inputColor={"bg-lime-200"}
-        setBgColor={props.setBgColor}
-        setInputColor={props.setInputColor}
-      />
-      <ColorSelectButton
-        bgColor={"bg-teal-300"}
-        inputColor={"bg-teal-200"}
-        setBgColor={props.setBgColor}
-        setInputColor={props.setInputColor}
-      />
-      <ColorSelectButton
-        bgColor={"bg-sky-300"}
-        inputColor={"bg-sky-200"}
-        setBgColor={props.setBgColor}
-        setInputColor={props.setInputColor}
-      />
-      <ColorSelectButton
-        bgColor={"bg-violet-300"}
-        inputColor={"bg-violet-200"}
-        setBgColor={props.setBgColor}
-        setInputColor={props.setInputColor}
-      />
-      <ColorSelectButton
-        bgColor={"bg-pink-300"}
-        inputColor={"bg-pink-200"}
-        setBgColor={props.setBgColor}
-        setInputColor={props.setInputColor}
-      />
-      <ColorSelectButton
-        bgColor={"bg-red-300"}
-        inputColor={"bg-red-200"}
-        setBgColor={props.setBgColor}
-        setInputColor={props.setInputColor}
-      />
+      {colors.map((color) => (
+        <ColorSelectButton
+          key={color.bgColor}
+          bgColor={color.bgColor}
+          inputColor={color.inputColor}
+          setBgColor={props.setBgColor}
+          setInputColor={props.setInputColor}
+        />
+      ))}
     </div>
   )
 }
