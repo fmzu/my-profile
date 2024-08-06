6c45835eae2c538b2984c6796d770f54eea31311
fmzu <fjtmzk0723@gmail.com>
2024-08-06 13:39:53 +0900

    update: ダイアログを追加する

--- app/routes/_componets/profile-image.tsx
+++ app/routes/_componets/profile-image.tsx
@@ -1,29 +1,38 @@
 import * as htmlToImage from "html-to-image"
+import { useState } from "react"
+import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog"
 
 export function ProfileImage() {
-  return (
-    <button
-      type="button"
-      onClick={() => {
-        const node = document.getElementById("profile")
+  const [imageDataUrl, setImageDataUrl] = useState<string | null>(null)
 
-        if (node) {
-          htmlToImage
-            .toPng(node)
-            .then((dataUrl) => {
-              const img = new Image()
-              img.src = dataUrl
-              document.body.appendChild(img)
-            })
-            .catch((error) => {
-              console.error("oops, something went wrong!", error)
-            })
-        } else {
-          console.error("Element with id 'profile' not found.")
-        }
-      }}
-    >
-      {"画像をつくる"}
-    </button>
+  const handleOnClick = (node: HTMLElement | null) => {
+    if (node) {
+      htmlToImage
+        .toPng(node)
+        .then((dataUrl) => {
+          setImageDataUrl(dataUrl)
+        })
+        .catch((error) => {
+          console.error("oops, something went wrong!", error)
+        })
+    } else {
+      console.error("Element with id 'profile' not found.")
+    }
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <button
+          type="button"
+          onClick={() => handleOnClick(document.getElementById("profile"))}
+        >
+          {"画像をつくる"}
+        </button>
+      </DialogTrigger>
+      <DialogContent className="bg-white">
+        {imageDataUrl && <img src={imageDataUrl} alt="Generated Profile" />}
+      </DialogContent>
+    </Dialog>
   )
 }
