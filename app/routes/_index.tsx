d2337d089104851d691e2b47a9b9c94673c6829d
fmzu <fjtmzk0723@gmail.com>
2024-08-20 14:55:23 +0900

    update: ページを修正する

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,7 @@ import { ProfileForm } from "./_componets/profile-form"
 import { ProfileImage } from "./_componets/profile-image"
 import { FontSelects } from "./_componets/font-selects"
 import { Header } from "./_componets/header"
+import { ProfilePaper } from "./_componets/profile-paper"
 
 export const meta: MetaFunction = () => {
   return [
@@ -24,17 +25,27 @@ export default function Index() {
   return (
     <div className="flex flex-col space-y-4">
       <Header />
-      <div className="flex flex-col items-center space-y-2">
-        <ColorSelects
-          bgColor={bgColor}
-          setBgColor={setBgColor}
-          inputColor={inputColor}
-          setInputColor={setInputColor}
-        />
-        <FontSelects font={font} setFont={setFont} />
-        <ProfileForm className={bgColor} inputColor={inputColor} font={font} />
-        <ProfileImage />
+      <div className="flex flex-col space-x-2 space-y-4 md:flex-row">
+        <div className="flex flex-col items-center space-y-2 md:w-1/2">
+          <ColorSelects
+            bgColor={bgColor}
+            setBgColor={setBgColor}
+            inputColor={inputColor}
+            setInputColor={setInputColor}
+          />
+          <FontSelects font={font} setFont={setFont} />
+          <ProfileForm
+            className={bgColor}
+            inputColor={inputColor}
+            font={font}
+          />
+          <ProfileImage />
+        </div>
+        <div className="flex items-center justify-center md:w-1/2">
+          <ProfilePaper />
+        </div>
       </div>
+      <Header />
     </div>
   )
 }
