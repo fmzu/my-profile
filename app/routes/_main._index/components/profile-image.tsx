c071b5e0232d8b1cd19514ba6282fe4e41bef049
fmzu <fjtmzk0723@gmail.com>
2024-08-22 12:17:16 +0900

    update: ページを修正する

--- app/routes/_main._index/components/profile-image.tsx
+++ app/routes/_main._index/components/profile-image.tsx
@@ -5,16 +5,14 @@ import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog"
 export function ProfileImage() {
   const [imageDataUrl, setImageDataUrl] = useState<string | null>(null)
 
-  const handleOnClick = (node: HTMLElement | null) => {
+  const handleOnClick = async (node: HTMLElement | null) => {
     if (node) {
-      htmlToImage
-        .toPng(node)
-        .then((dataUrl) => {
-          setImageDataUrl(dataUrl)
-        })
-        .catch((error) => {
-          console.error("oops, something went wrong!", error)
-        })
+      try {
+        const dataUrl = await htmlToImage.toPng(node)
+        setImageDataUrl(dataUrl)
+      } catch (error) {
+        console.error("oops, something went wrong!", error)
+      }
     } else {
       console.error("Element with id 'profile' not found.")
     }
--- app/routes/_main._index/components/save-button.tsx
+++ app/routes/_main._index/components/save-button.tsx
@@ -6,16 +6,14 @@ import { Dialog, DialogContent, DialogTrigger } from "~/components/ui/dialog"
 export function SaveButton() {
   const [imageDataUrl, setImageDataUrl] = useState<string | null>(null)
 
-  const handleOnClick = (node: HTMLElement | null) => {
+  const handleOnClick = async (node: HTMLElement | null) => {
     if (node) {
-      htmlToImage
-        .toPng(node)
-        .then((dataUrl) => {
-          setImageDataUrl(dataUrl)
-        })
-        .catch((error) => {
-          console.error("oops, something went wrong!", error)
-        })
+      try {
+        const dataUrl = await htmlToImage.toPng(node)
+        setImageDataUrl(dataUrl)
+      } catch (error) {
+        console.error("oops, something went wrong!", error)
+      }
     } else {
       console.error("Element with id 'profile' not found.")
     }
@@ -32,7 +30,7 @@ export function SaveButton() {
         </Button>
       </DialogTrigger>
       <DialogContent className="w-96 bg-white">
-        <p className="font-bold"> {"保存"}</p>
+        <p className="font-bold"> {"設定"}</p>
         {imageDataUrl && <img src={imageDataUrl} alt="Generated Profile" />}
       </DialogContent>
     </Dialog>
