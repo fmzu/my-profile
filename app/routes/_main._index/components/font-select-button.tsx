059e8c3cedb10f34b2a8853862124e5e66ab133c
fmzu <u1900050@gmail.com>
2024-09-07 23:56:52 +0900

    update: 洗濯したものがわかるようにする

--- app/routes/_main._index/components/font-select-button.tsx
+++ app/routes/_main._index/components/font-select-button.tsx
@@ -3,12 +3,19 @@ import { Button } from "~/components/ui/button"
 type Props = {
   font: string
   setFont: (color: string) => void
+  selectedFont: string
 }
 
 export function FontSelectButton(props: Props) {
+  const isSelected = props.font === props.selectedFont
+
+  const getButtonClass = () => {
+    return isSelected ? "border-2 border-black" : ""
+  }
+
   return (
     <Button
-      className={"content-center items-center text-xs"}
+      className={`content-center items-center text-xs ${getButtonClass()}`}
       onClick={() => {
         props.setFont(props.font)
       }}
